c7b0ace1e2a57417caece7345be6d99a8649700a
pudovana <124800948+pudovana@users.noreply.github.com>
2023-06-09 14:45:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -39,15 +39,5 @@ def calc(loan_amount, interest_rate, loan_term):
     else:
         return render_template
 
-class test_Converter(unittest.TestCase):
-    def test_values(self):
-        self.assertEqual(calc(100000, 5, 12))
-        self.assertEqual(calc(-100000, -1, -2), "Одно или все значения меньше нуля")
-
-    def test_types(self):
-        self.assertEqual(calc(100000, 1, "пять"), 'ValueError: не является числом')
-        self.assertEqual(calc(100000, 1, [1, 2, 3]), 'TypeError: недопустимое значение')
-        self.assertEqual(calc(100000, 1, (1, 2, 3)), 'TypeError: недопустимое значение')
-
 if __name__ == '__main__':
     app.run(debug=True)
