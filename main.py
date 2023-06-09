ffdd7a5f247becd26b0d33a0e28a5dab860348b9
pudovana <124800948+pudovana@users.noreply.github.com>
2023-06-09 14:47:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-from flask import Flask, render_template, request
-import unittest
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    loan_amount = float(request.form['loan_amount'])
-    interest_rate = float(request.form['interest_rate']) / 100
-    loan_term = int(request.form['loan_term'])
-
-    monthly_interest_rate = interest_rate / 12
-    total_payments = loan_term * 12
-
-    monthly_payment = (loan_amount * monthly_interest_rate) / (1 - (1 + monthly_interest_rate) ** (-total_payments))
-    total_cost = monthly_payment * total_payments
-    total_interest = total_cost - loan_amount
-
-    return render_template('result.html', monthly_payment=monthly_payment, total_cost=total_cost,
-                           total_interest=total_interest)
 
 def calc(loan_amount, interest_rate, loan_term):
     try:
@@ -38,6 +12,3 @@ def calc(loan_amount, interest_rate, loan_term):
         return "Одно или все значения меньше нуля"
     else:
         return render_template
-
-if __name__ == '__main__':
-    app.run(debug=True)
